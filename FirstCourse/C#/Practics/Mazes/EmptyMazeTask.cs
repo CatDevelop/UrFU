8dfd12c2949d21b66a16b8236456b17e959cc408
CatDeveloper <maks.rozhkov.2004@gmail.com>
2022-10-15 12:41:11 +0500

    refactor: change structure of the project

--- FirstCourse/C#/Practics/Mazes/EmptyMazeTask.cs
+++ FirstCourse/C#/Practics/Mazes/EmptyMazeTask.cs
@@ -2,28 +2,24 @@
 {
 	public static class EmptyMazeTask
 	{
-		/// <param name="robot">Объект робота</param>
-		/// <param name="stepCount">Количество шагов</param>
-		/// <returns>Двигает робота вправо на stepCount шагов</returns>
-		private static void MoveRight(Robot robot, int stepCount)
-		{
-			for(var i=0; i<stepCount; i++)
-				robot.MoveTo(Direction.Right);
-		}
-		
-		/// <param name="robot">Объект робота</param>
-		/// <param name="stepCount">Количество шагов</param>
-		/// <returns>Двигает робота вниз на stepCount шагов</returns>
-		private static void MoveDown(Robot robot, int stepCount)
-		{
-			for(var i=0; i<stepCount; i++)
-				robot.MoveTo(Direction.Down);
-		}
-		
 		public static void MoveOut(Robot robot, int width, int height)
 		{
-			MoveDown(robot,height - 2 - robot.Y);
-			MoveRight(robot, width - 2 - robot.X);
+			MoveRobot(robot,height - 2 - robot.Y, Direction.Down);
+			MoveRobot(robot, width - 2 - robot.X, Direction.Right);
+		}
+		
+		/// <param name="robot">Объект робота</param>
+		/// <param name="stepCount">Количество шагов</param>
+		/// <param name="moveDirection">Направление движения</param>
+		/// <returns>Двигает робота на stepCount шагов в moveDirection сторону,
+		/// если он ещё не дошёл до конца</returns>
+		private static void MoveRobot(Robot robot, int stepCount, Direction moveDirection)
+		{
+			if (!robot.Finished)
+			{
+				for (var i = 0; i < stepCount; i++)
+					robot.MoveTo(moveDirection);
+			}
 		}
 	}
 }
