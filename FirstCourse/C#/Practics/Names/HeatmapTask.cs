c4424a351814d83749cb4d29435bc7d3c6db54c4
CatDeveloper <maks.rozhkov.2004@gmail.com>
2022-10-15 12:26:50 +0500

    feat: add names project

---
+++ FirstCourse/C#/Practics/Names/HeatmapTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        private const int Days = 30;
+        private const int Months = 12;
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var xLabels = GetLabel(2, Days);
+            var yLabels = GetLabel(2, Months);
+            var heatMap = new double[Days,Months];
+
+            foreach (var person in names)
+            {
+                if (person.BirthDate.Day != 1)
+                    heatMap[person.BirthDate.Day-2, person.BirthDate.Month-1] += 1;
+            }
+            return new HeatmapData(
+                "Тепловая карта", heatMap, xLabels, yLabels);
+        }
+
+        private static string[] GetLabel(int start, int end)
+        {
+            var label = new string[end]; 
+            for (var i = 0; i < end; i++)
+                label[i] = Convert.ToString(i + start);
+            return label;
+        }
+    }
+}
---
+++ FirstCourse/C#/Practics/Names/HistogramTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        private const int Days = 31;
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var xLabels = new string[Days];
+            var yValues = new double[Days];
+            for (var i = 0; i < Days; i++)
+                xLabels[i] = Convert.ToString(i + 1);
+            foreach (var person in names)
+            {
+                if (person.Name == name && person.BirthDate.Day != 1)
+                    yValues[person.BirthDate.Day-1] += 1;
+            }
+            return new HistogramData($"Рождаемость людей с именем '{name}'", xLabels, yValues);
+        }
+    }
+}
